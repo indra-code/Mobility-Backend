157b2a8b0ccf56f6f4917564a530342ec8bc7082
indra-code <reachme.neel@gmail.com>
2024-09-29 09:39:05 +0530

    second

--- backend/test.py
+++
@@ -1,8 +0,0 @@
-import requests
-BASE = 'http://127.0.0.1:5000/'
-img_file_path = "image.jpg"
-response = requests.post(
-    url=BASE+'upload-image',
-    files={'image': open(img_file_path,'rb')}
-)
-print(response.json())
